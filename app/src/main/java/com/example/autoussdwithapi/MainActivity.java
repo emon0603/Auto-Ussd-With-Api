2f36e5c706c55690012e8b432ca9722a9b156180
Bangladeshi Software <emonhossain6373@gmail.com>
2024-10-15 17:44:39 +0600

    First Commit

--- app/src/main/java/com/example/autoussdwithapi/MainActivity.java
+++ app/src/main/java/com/example/autoussdwithapi/MainActivity.java
@@ -2,24 +2,72 @@ package com.example.autoussdwithapi;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar setup
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        // DrawerLayout setup
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // ActionBarDrawerToggle for hamburger icon
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // If no saved state, show home fragment by default
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Close drawer if open, else handle back press as usual
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks
+
+        if (item.getItemId() == R.id.nav_home){
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
+        } else if (item.getItemId() ==R.id.nav_settings ) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new AboutFragment()).commit();
+
+        }
+
+
+        // Close drawer after selecting an item
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
