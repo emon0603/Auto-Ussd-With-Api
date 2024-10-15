d67e474e151755be83b2ec1c1e061f6f887259df
Rakibul Emon <emonhossain6373@gmail.com>
2024-10-15 22:51:29 +0600

    Drawer Navigration with Fragment

--- app/src/main/java/com/example/autoussdwithapi/MainActivity.java
+++ app/src/main/java/com/example/autoussdwithapi/MainActivity.java
@@ -2,17 +2,27 @@ package com.example.autoussdwithapi;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
     private DrawerLayout drawerLayout;
+    private MaterialToolbar toolbar;
+
+    private FrameLayout fragment;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,54 +30,43 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         // Toolbar setup
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // DrawerLayout setup
+        toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView = findViewById(R.id.nav_view);
+        fragment = findViewById(R.id.content_frame);
 
-        // ActionBarDrawerToggle for hamburger icon
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_close,
+                R.string.navigation_drawer_open
+        );
+
         drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
 
-        // If no saved state, show home fragment by default
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
-        }
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.nav_home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else if (item.getItemId() ==R.id.nav_settings ) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new AboutFragment()).commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+
+
+                return false;
+            }
+        });
+
+
     }
 
-    @Override
-    public void onBackPressed() {
-        // Close drawer if open, else handle back press as usual
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks
-
-        if (item.getItemId() == R.id.nav_home){
-            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new HomeFragment()).commit();
-        } else if (item.getItemId() ==R.id.nav_settings ) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
-        } else {
-            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new AboutFragment()).commit();
-
-        }
-
-
-        // Close drawer after selecting an item
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
