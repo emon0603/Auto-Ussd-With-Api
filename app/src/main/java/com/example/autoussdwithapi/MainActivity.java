c975e6d1dda642797fe7efdffdcd36f6a8d8007e
Bangladeshi Software <emonhossain6373@gmail.com>
2024-10-15 17:21:31 +0600

    First Commit

---
+++ app/src/main/java/com/example/autoussdwithapi/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.autoussdwithapi;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
