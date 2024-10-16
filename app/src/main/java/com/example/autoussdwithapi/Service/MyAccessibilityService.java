fb7e5a208c3393fb253c556103bc5428e22089a2
Rakibul Emon <emonhossain6373@gmail.com>
2024-10-16 07:05:48 +0600

    Accessibility Permission

---
+++ app/src/main/java/com/example/autoussdwithapi/Service/MyAccessibilityService.java
@@ -0,0 +1,16 @@
+package com.example.autoussdwithapi.Service;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
